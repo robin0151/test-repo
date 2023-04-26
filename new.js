fff37d0f314239327b90a3ecd90bacf311572823
Ahammed Robin <ahmedrobin1952@gmail.com>
2023-04-26 16:04:00 +0600

    change code

--- new.js
+++ new.js
@@ -1,2 +1,5 @@
-var sum = 5 * 5;
-console.log(sum);
+var mangoPrice = "500.50";
+var quantity = 9;
+var mangoPrice = parseFloatg(mangoPrice);
+var remainder = mangoPrice % quantity;
+console.log(remainder);
