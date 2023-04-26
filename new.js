c3577e58716331494ffbaefe115c0081b94acace
Ahammed Robin <ahmedrobin1952@gmail.com>
2023-04-26 16:12:34 +0600

    name added

--- new.js
+++ new.js
@@ -1,5 +1,11 @@
 var mangoPrice = "500.50";
 var quantity = 9;
-var mangoPrice = parseFloatg(mangoPrice);
+var mangoPrice = parseFloat(mangoPrice);
 var remainder = mangoPrice % quantity;
 console.log(remainder);
+
+var firstName = 'Ahammed';
+var lastName = 'Robin';
+var fullName = firstName +' '+  lastName;
+console.log(fullName);
+
