b0c03fa38fab6cf2fde1d8e3af58a0d9ca88638a
Ahammed Robin <ahmedrobin1952@gmail.com>
2023-04-29 00:42:44 +0600

    added equation

--- new.js
+++ new.js
@@ -9,3 +9,13 @@ var lastName = 'Robin';
 var fullName = firstName +' '+  lastName;
 console.log(fullName);
 
+var price = 50;
+var quantity = 10;
+var totalPrice = price / quantity;
+console.log(totalPrice);
+
+var a = 10;
+var b = 20;
+var c = a + b;
+
+console.log(a + ' + ' + b + ' = ' + c);
